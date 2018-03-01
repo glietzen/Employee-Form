6bc216398a12d0941fe722debd1347531dc1adc8
Garrett Lietzen <glietzen@gmail.com>
2018-02-28 19:33:20 -0600

    connected js file.

--- form.js
+++ form.js
@@ -7,11 +7,14 @@ $(document).ready(function () {
     var monthlyRate = '';
 
     $('#submit-button').on('click', function() {
+        event.preventDefault();
 
         employeeName = $('#name-input').val().trim();
         role = $('#role-input').val().trim();
-        startDate = $('#start-input').val().trim();
-        monthlyRate = $('#monthly-input').val().trim();
+        startDate = $('#start-input').val()//.trim();
+        monthlyRate = $('#monthly-input').val()//.trim();
+
+        console.log(employeeName, role, startDate, monthlyRate);
 
         newRow = '<tr><td>' + employeeName + '</td><td>' + role + '</td><td>' + '<td>' + startDate + '</td><td>' + '<td>' + monthlyRate + '</td><td>'
 
