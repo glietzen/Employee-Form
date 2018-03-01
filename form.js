748a2f5c05790f146c0cfbe0317423a4dab02844
Garrett Lietzen <glietzen@gmail.com>
2018-02-28 20:23:47 -0600

    new stuff

--- form.js
+++ form.js
@@ -1,46 +1,59 @@
 $(document).ready(function () {
 
-       
-        // Initialize Firebase
-        var config = {
-                apiKey: "AIzaSyCWHVsprbzdNVZE21_iII6YwDs3vaTr2m8",
-                authDomain: "click-counter-63e56.firebaseapp.com",
-                databaseURL: "https://click-counter-63e56.firebaseio.com",
-                projectId: "click-counter-63e56",
-                storageBucket: "click-counter-63e56.appspot.com",
-                messagingSenderId: "305383994479"
-        };
-        firebase.initializeApp(config);
 
-        var database = firebase.database();
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyCWHVsprbzdNVZE21_iII6YwDs3vaTr2m8",
+        authDomain: "click-counter-63e56.firebaseapp.com",
+        databaseURL: "https://click-counter-63e56.firebaseio.com",
+        projectId: "click-counter-63e56",
+        storageBucket: "click-counter-63e56.appspot.com",
+        messagingSenderId: "305383994479"
+    };
+    firebase.initializeApp(config);
 
-        var newRow = '';
-        var employeeName = '';
-        var role = '';
-        var startDate = '';
-        var monthlyRate = '';
+    var database = firebase.database();
 
-        $('#submit-button').on('click', function () {
-                event.preventDefault();
+    var newRow = '';
+    var employeeName = '';
+    var role = '';
+    var startDate = '';
+    var monthlyRate = '';
 
-                employeeName = $('#name-input').val().trim();
-                role = $('#role-input').val().trim();
-                startDate = $('#startDate-input').val();
-                monthlyRate = $('#rate-input').val();
+    $('#submit-button').on('click', function () {
+        event.preventDefault();
 
-                
+        employeeName = $('#name-input').val().trim();
+        role = $('#role-input').val().trim();
+        startDate = $('#startDate-input').val();
+        monthlyRate = $('#rate-input').val();
 
-                console.log(employeeName, role, startDate, monthlyRate);
 
-                database.ref('/employeeData').push({
-                        employeeName: employeeName,
-                        role: role,
-                        startDate: startDate,
-                        monthlyRate: monthlyRate
-                });
 
-                newRow = '<tr><td>' + employeeName + '</td><td>' + role + '</td><td>' + startDate + '</td><td>' + monthlyRate + '</td>';
+        console.log(employeeName, role, startDate, monthlyRate);
 
-                $('#table-body').append(newRow);
-        })
+        database.ref('/employeeData').push({
+            employeeName: employeeName,
+            role: role,
+            startDate: startDate,
+            monthlyRate: monthlyRate
+        });
+
+        // newRow = '<tr><td>' + employeeName + '</td><td>' + role + '</td><td>' + startDate + '</td><td>' + monthlyRate + '</td>';
+
+        $('#table-body').append(newRow);
+    })
+
+    function getData () {
+        database.ref('/employeeData').on('child_added', function (snapshot) {
+            employeeName = snapshot.val().name;
+            role = snapshot.val().role;
+            startDate = snapshot.val().startDate;
+            monthlyRate = snapshot.val().monthlyRate;
+            newRow = '<tr><td>' + employeeName + '</td><td>' + role + '</td><td>' + startDate + '</td><td>' + monthlyRate + '</td>';
+            $('#table-body').append(newRow);
+            console.log(newRow);
+
+        })}
+        getData();
 });
